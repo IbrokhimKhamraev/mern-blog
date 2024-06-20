01b2b19452c2c12039b1148706f564951ed27c99
Ibrokhim Khamraev <i.khamraev0095@gmail.com>
2024-06-20 13:31:29 +0500

    20 June 13:30

--- api/index.js
+++ api/index.js
@@ -7,11 +7,20 @@ import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
 import cookieParser from "cookie-parser";
+import path from 'path'
+
+
+const __dirname = path.resolve()
 
 const app = express()
 
-app.use(express.json({limit: '50mb'}))
-app.use(express.urlencoded({limit: '50mb'}))
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
+app.use(express.json())
 app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO)
