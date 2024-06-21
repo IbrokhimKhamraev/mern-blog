d7fe8fc9b5dfd9725f3616a157f08ff89336da29
Ibrokhim Khamraev <i.khamraev0095@gmail.com>
2024-06-21 15:53:31 +0500

    21 June 15:55

--- api/index.js
+++ api/index.js
@@ -15,23 +15,18 @@ const __dirname = path.resolve()
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
-
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
 
 app.use(express.json())
 app.use(cookieParser())
 
 
 mongoose.connect(process.env.MONGO)
-   .then(() => {
-      console.log(`MongoDB is connected`);
-   })
-   .catch((err) => {
-      console.log(err);
-   })
+.then(() => {
+   console.log(`MongoDB is connected`);
+})
+.catch((err) => {
+   console.log(err);
+})
 
 app.listen(3000, () => {
    console.log('Server is running on port 3000');
@@ -42,6 +37,14 @@ app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 
 app.use((err, req, res, next) => {
    const statusCode = err.statusCode || 500
